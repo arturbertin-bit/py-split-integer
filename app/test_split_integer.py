3faa9451b635b8c82c3231955fa42e3e336921fc
Artur Bertin <arturbertin2006@gmail.com>
2026-01-20 10:27:42 -0300

    Update test_split_integer.py

--- app/test_split_integer.py
+++ app/test_split_integer.py
@@ -2,30 +2,19 @@ from app.split_integer import split_integer
 
 
 def test_sum_of_the_parts_should_be_equal_to_value() -> None:
-    expected = 15
-    actual = sum(split_integer(15, 3))
-    assert actual == expected
-
+    assert sum(split_integer(15, 3)) == 15
 
 def test_should_split_into_equal_parts_when_value_divisible_by_parts() -> None:
-    expected = [2, 2, 2, 2]
-    actual = split_integer(8, 4)
-    assert actual == expected
+    assert split_integer(8, 4) == [2, 2, 2, 2]
 
 
 def test_should_return_part_equals_to_value_when_split_into_one_part() -> None:
-    expected = [8]
-    actual = split_integer(8, 1)
-    assert actual == expected
+    assert split_integer(8, 1) == [8]
 
 
 def test_parts_should_be_sorted_when_they_are_not_equal() -> None:
-    expected = [2, 2, 2, 2, 3, 3]
-    actual = split_integer(14, 6)
-    assert actual == expected
+    assert split_integer(14, 6) == [2, 2, 2, 2, 3, 3]
 
 
 def test_should_add_zeros_when_value_is_less_than_number_of_parts() -> None:
-    expected = [0, 0, 1, 1]
-    actual = split_integer(2, 4)
-    assert actual == expected
+    assert split_integer(2, 4) == [0, 0, 1, 1]
